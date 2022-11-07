50c969ce309d7b5f768165c45c0e598523bb6bc1
MarianaGb12 <98919850+MarianaGb12@users.noreply.github.com>
2022-11-07 18:04:54 -0500

    Create code_to_run.py

---
+++ code_to_run.py
@@ -0,0 +1,7 @@
+from Final_project import Register, Menu
+
+cuenta = Register(input("Ingrese su usuario: "),input("Ingrese constraseña:") )
+cuenta.iniciarsesion()
+
+search = Menu("")
+search.Buscar()
