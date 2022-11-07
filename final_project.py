704acd2793c857807050d78f19f1be821092b0d1
MarianaGb12 <98919850+MarianaGb12@users.noreply.github.com>
2022-11-07 18:02:33 -0500

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,3 +1,4 @@
+
 #%%
 class Register:
     num_usuarios = 0
@@ -11,7 +12,6 @@ class Register:
         
         Register.num_usuarios+=1
 
-        
     def iniciarsesion(self):
         nomusuario1 = input("Ingrese nombre de usuario:")
         contraseña1 = input("Ingrese contraseña:")
@@ -23,7 +23,7 @@ class Register:
             if self.intento > 0:
               print ("Error.Intentelo nuevamente")
               print("Usuario o contraseña incorrecto")
-              self.login()
+              self.iniciarsesion()
             else:
               print ("Inténtelo más tarde.")
 
@@ -33,80 +33,200 @@ class Register:
             print("Salida con éxito")
             self.conectar = False
             
-cuenta = Register(input("Ingrese su usuario: "),input("Ingrese constraseña:") )
-print(cuenta )
-cuenta.login()
-
 class Usuario (Register):
     def _init_(self, nomusuario, contraseña) -> None:
-        super().init(nomusuario, contraseña)
+        super().__init__(nomusuario, contraseña)
 
 # %%
 class Buscador:
-    def __init__(self, opcion:int):
-        self.opcion = opcion
+    def __init__(self, opcion1:int):
+        self.opcion1 = opcion1
 
 # %%
 class Menu(Buscador):
-    def __init__(self, opcion) -> None:
+    def __init__(self, opcion1) -> None:
         super().__init__(Buscador)
-    
-    def Buscar(self):
-        while(True):
-            print("* -Menú- * ")
-            print("1- Mostrar libros")
-            print("2- Buscar por titulo")
-            print("3- Buscar por categoría")
-            print("4- Ver recomendaciones")
-            print("5- Agregar a Libros Leídos")
-            print("6- Agregar a Quiero Leer")
-            print("7- Salir del menú")
-            opcion1=int(input("Escoja una opción: "))
-        
-            if opcion1==1:
-                print("*** LISTA DE LIBROS ***")
-                with open("libros1.txt") as archivo:
-                    print(archivo.readlines())
-                print("\n")
 
-            elif opcion1==2:
-                ...
+    def buscador(self):
+        # try catch except para manejar
+        # error la búsqueda
+        # iniciando bloque try
+        try:
+            # apertura del archivo de libros
+            # como lectura 
+            archivo1_buscar = open("libros.txt", "r")
+                
+            # ingreso del string (titulo,autor,categoria) que el 
+            # usuario desea buscar
+            searching = input("Ingrese título, autor o categoría: ")
+                
+            # lectura del archivo línea por línea
+            lines = archivo1_buscar.readlines()
+                
+            nueva_lista = []
+            idx = 0
+                
+            # looping en cada línea del archivo
+            for line in lines:
+                # si la línea tiene la palabra ingresada, toma
+                # la posición de la línea y la coloca
+                # en una nueva lista 
+                if searching in line:
+                    nueva_lista.insert(idx, line)
+                    idx += 1
 
-            elif opcion1==3:
-                ...
-
-            elif opcion1==4:
-                ...
+            # cierre del archivo
+            archivo1_buscar.close()
             
-            elif opcion1==5:
-                print("Agregar libro a Estante Leídos")
+            # si el lenght de la nueva lista es 0
+            # el input no se encuentra en el archivo 
+            if len(nueva_lista)==0:
+                print("\n\"" +searching+ "\" no se encuentra en \"" +archivo1_buscar+ "\"!")
+            else:
+                # visualizar líneas que  
+                # contienen el input ingresado
+                lineLen = len(nueva_lista)
+                print("\n - Buscando \"" +searching+ "\"- \n")
+                for i in range(lineLen):
+                    print(end=nueva_lista[i])
+                print()
+                
+        # iniciando bloque except 
+        except :
+            print("\n Error en la búsqueda")
+    
+    def estante_leidos(self):
+
+        print("\n*** Estante LIBROS LEÍDOS ***")
+        while (True):
+            print("1- Agregar libro a leídos || 2-Ver estante LIBROS LEÍDOS || 3-Atrás")
+            opcion2 = int(input("Escoja una opción: "))
+
+            if opcion2 == 1:  
+                # apertura del archivo en modo escritura
+                archivo_leidos = open("leidos.txt", "w")
+                        
+                print("* Agregar libro a Estante Leídos *")
                 librosleidos = []
-                nuevolibro = input("¿Desea añadir un libro a leídos: 1)Si o 2)No")
+                nuevolibro = input(" 1-Añadir libro || 2-Regresar : ")
                 while nuevolibro == "1":
                     librousuario = input("Ingrese el título: ")
                     librosleidos.append(librousuario)
-                    nuevolibro = input("¿Desea añadir más libros: 1)Si , 2)No")
-                
+                    nuevolibro = input("¿Desea añadir más libros: 1.Si , 2.No :")
                 print(librosleidos)
 
-            elif opcion1==6:
+                # recorremos la lista y agregamos cada titulo al archivo
+                for leido in librosleidos:
+                    print(leido, file=archivo_leidos)
+                # cierre del archivo
+                archivo_leidos.close()
+
+            elif opcion2 == 2:
+                print("* LIBROS LEÍDOS *")
+                with open("leidos.txt") as archivo:
+                    print(archivo.readlines())
+                print("\n")
+
+            elif opcion2 == 3:
+                break
+            else:
+                print("Opción inválida")
+
+    def estante_quieroleer(self):
+
+        print("\n*** Estante QUIERO LEER ***")
+        while (True):
+            print("1- Agregar libro a Quiero Leer || 2-Ver estante QUIERO LEER || 3-Atrás")
+            opcion3 = int(input("Escoja una opción: "))
+            if opcion3 == 1:
+                # apertura del archivo en modo escritura
+                archivo_quieroleer = open("quiero_leer.txt", "w")
+                        
                 print("Agregar libro a Estante Quiero Leer")
                 wanttoread = []
-                nuevlibro = input("¿Desea añadir un libro a Quiero Leer: 1)Si , 2)No")
+                nuevlibro = input(" 1-Añadir libro || 2-Regresar ")
                 while nuevlibro == "1":
                     libro_usuario = input("Ingrese título: ")
                     wanttoread.append(libro_usuario)
                     nuevlibro = input("¿Desea añadir más libros: 1)Si , 2)No")
                 print(wanttoread)
 
-            elif opcion1==7:
+                # Recorremos la lista y agregamos cada titulo al archivo
+                for want in wanttoread:
+                    print(want, file=archivo_quieroleer)
+                # cierre del archivo
+                archivo_quieroleer.close()
+
+            elif opcion3 == 2:
+                print("* QUIERO LEER *")
+                with open("quiero_leer.txt") as archivo:
+                    print(archivo.readlines())
+                print("\n")
+
+            elif opcion3 == 3:
+                break
+
+            else:
+                print("Opción inválida")
+                
+    def Buscar(self):
+        while(True):
+            print("\n* -Menú- * ")
+            print("1- Mostrar libros")
+            print("2- Buscador")
+            print("3- Ver recomendaciones")
+            print("4- Estante Libros Leídos")
+            print("5- Agregar a Quiero Leer")
+            print("6- Salir del menú")
+            opcion1=int(input("Escoja una opción: "))
+        
+            if opcion1 == 1:
+                print("\n*** LISTA DE LIBROS ***")
+                with open("libros1.txt") as archivo:
+                    print(archivo.readlines())
+                print("\n")
+                    
+                while(True):
+                    print("Desea agregar libros a su estante ")
+                    print("1)Si || 2)No : ")
+                    op = int(input("Escoja una opción: \n"))
+                    if op == 1:
+                        print("\n1)Estantería LIBROS LEÍDOS || 2)Estantería QUIERO LEER || 3) Atrás  ")
+                        op2= int(input("Elija una opción : "))
+
+                        if op2 == 1: 
+                            self.estante_leidos()
+
+                        elif op2 == 2:
+                            self.estante_quieroleer()
+                            
+                        elif op2 == 3:
+                            break
+
+                        else:
+                            print("Opción inválida")
+                    elif op == 2:
+                        break
+
+                    else:
+                        print("Opción inválida")
+
+            elif opcion1 == 2:
+                print("*** Buscador ***")
+                self.buscador()
+            
+            elif opcion1 == 3:
+                ...
+
+            elif opcion1 == 4:
+                self.estante_leidos()
+
+            elif opcion1 == 5:
+                self.estante_quieroleer()
+
+            elif opcion1 == 6:
                 break
 
             else:
                 print("Opción no válida")
-            
-
-            
-search = Menu("")
-search.Buscar()
-
+                
