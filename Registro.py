e444ce0941ed1907081cb0a8732fe479ea10b676
sergio110304 <105168888+sergio110304@users.noreply.github.com>
2022-11-12 09:31:07 -0500

    Update Registro.py

--- Registro.py
+++ Registro.py
@@ -1,39 +1,39 @@
 from Menu_secundario import Menu_secundario
 
+
 class Usuario:
     def __init__(self):
         pass
-    
+
     def Registro(self):
         print("Nuevo registro. Introduza sus datos ")
         self.nomusuario = input("Nombre de usuario:")
         self.contraseña = input("Contraseña:")
         self.Registro_usuario()
-    
+
     def Registro_usuario(self):
         # Lectura de archivo, para verificar que no exista el usuario
-        file = open("usuarios.txt", "r") 
+        file = open("usuarios.txt", "r")
         info = file.read()
         if self.nomusuario in info:
             return "Usuario ya existe. Por favor intente nuevamente"
         file.close()
 
         # CREACION DE Archivo CON "nombre" y "contraseña" del usuario
-        file = open("usuarios.txt", "w") 
+        file = open("usuarios.txt", "w")
         info = (info + " " + self.nomusuario + " " + self.contraseña)
         file.write(info)
         file.close()
         self.exito_registro()
-        
 
     def Verifica_login(self):
         print("Acceso a la cuenta. Introduza sus datos ")
         self.nomusuario = (input("Nombre de usuario:"))
         self.contraseña = (input("Contraseña:"))
-        
+
         # CREACION DE ARCHIVO CON "nombre" y "contraseña" en modo lectura
-        file = open("usuarios.txt", "r") 
-        leer = file.read() 
+        file = open("usuarios.txt", "r")
+        leer = file.read()
         leer = leer.split()
 
         """
@@ -64,12 +64,12 @@ class Usuario:
     # "El usuario se ha registrado correctamente"
     def exito_registro(self):
         print("Registro completado con éxito")
-        
 
     # "Contraseña incorrecta".
+
     def no_cont(self):
         print("Contraseña incorrecta ")
- 
+
     # "Usuario no encontrado".
     def no_usuario(self):
         print("Usuario no encontrado")
